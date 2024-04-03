5a3ec890cae840416792bb1529f19eeda4091da3
imhaseeb95 <145844952+imhaseeb95@users.noreply.github.com>
2024-04-03 16:45:35 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 'use strict';
 
+$('.testimonials-slider').slick({
+	centerMode: true,
+	infinite: true,
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	arrows: false,
+	dots:true,
+	autoplay: true,
+    autoplaySpeed: 2000,
+	responsive: [
+	  {
+		breakpoint: 768,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 1
+		}
+	  }
+	]
+  });
+
+
+
+
 // ------ Header --------
 var navBar = document.querySelector('header');
 // var headerScroll = document.querySelector("header.sec-header");
